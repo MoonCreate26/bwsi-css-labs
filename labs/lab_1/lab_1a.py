dbb576329990f405b33d2da99d4daedc83e84524
Moon Create <mricky5555@gmail.com>
2026-03-17 20:42:57 -0500

    Added intro to lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Riku Maekawa" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hi")
 
 if __name__ == "__main__":
     main()
